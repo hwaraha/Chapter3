c495fecf9a445c7977be6e8c403b53ec9e96570b
hwaraha <114084952+hwaraha@users.noreply.github.com>
2022-09-26 04:14:45 +0900

    Add files via upload

---
+++ midterm.py
@@ -0,0 +1,19 @@
+# Spring semester
+for celsius in range(0, 51, 5):
+    Fahrenheit = (9 / 5) * celsius + 32
+    print(celsius, int(Fahrenheit))
+
+a = 2*2//2
+b = 3//2*3
+print(a, b)
+
+sum = 0
+for i in range(1, 1000):
+  if i % 3 == 0 or i % 5 == 0:
+      sum += i
+print(sum)
+
+
+
+
+
